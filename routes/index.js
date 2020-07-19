c33a661344bbc708f8316223894c01f8774131fa
sabercyber <42636984+sabercyber@users.noreply.github.com>
2020-07-19 22:19:54 +0530

    Add files via upload

---
+++ routes/index.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const router = express.Router()
+const {ensureAuth, ensureGuest} = require('../middleware/auth')
+
+
+const Story = require ('../models/story')
+
+//@deck Login and Landing page
+//@route Get/
+
+router.get('/', ensureGuest,(req,res) => {
+    res.render('login',{
+        layout: 'login',
+    })
+})
+
+
+//@deck Dashboard
+//@route Get /dashboard
+
+router.get('/dashboard',ensureAuth , async(req,res) => {
+    
+    try{
+        const stories = await Story.find({user:req.user.id}).lean()
+        res.render('dashboard', {
+        name: req.user.firstName,
+        stories,
+          })
+
+    }catch(err){
+        console.log(err)
+        res.render('error/500')
+
+    }
+    
+})
+
+module.exports = router
